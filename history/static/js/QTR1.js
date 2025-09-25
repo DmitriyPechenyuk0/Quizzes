a2694d33fef9db6483779fab753211d6887c7ee1
KirillKharlan <151565579+KirillKharlan@users.noreply.github.com>
2025-09-25 21:50:58 +0300

    qtrupd

--- history/static/js/QTR1.js
+++ history/static/js/QTR1.js
@@ -1,23 +1,32 @@
+const quests = {
+    firstQuestion: 100,
+    secondQuestion: 50,
+    thirdQuestion: 0,
+    fourQuestion: 100,
+}
+
+
+
 var myChart = echarts.init(document.getElementById('main'));
 
-                    var option = {
-                        title: {
-                        text: 'Результати групи з питань'
-                        },
-                        tooltip: {},
-                        legend: {
-                        data: ['questions']
-                        },
-                        xAxis: {
-                        data: ['Питання 1', 'Питання 2', 'Питання 3', 'Питання 4']
-                        },
-                        yAxis: {},
-                        series: [
-                        {
-                            type: 'bar',
-                            data: [100, 50, 0, 100]
-                        }
-                        ]
-                    };
+var option = {
+    title: {
+    text: 'Результати групи з питань'
+    },
+    tooltip: {},
+    legend: {
+    data: ['questions']
+    },
+    xAxis: {
+    data: ['Питання 1', 'Питання 2', 'Питання 3', 'Питання 4']
+    },
+    yAxis: {},
+    series: [
+    {
+        type: 'bar',
+        data: [quests.firstQuestion, quests.secondQuestion, quests.thirdQuestion, quests.fourQuestion]
+    }
+    ]
+};
 
-                    myChart.setOption(option);
+myChart.setOption(option);
--- history/static/js/QTR2.js
+++ history/static/js/QTR2.js
@@ -1,47 +1,55 @@
+const answers = {
+    correct: 60,
+    uncorrect: 40
+}
+
+
+
+
 
 
 var myChart = echarts.init(document.getElementById('main-2'));
 
-                    var option = {
-                        title: {
-                            text: 'Результати усіх питань',
-                            left: 'center'
-                        },
-                        tooltip: {
-                            trigger: 'item'
-                        },
+var option = {
+title: {
+    text: 'Результати усіх питань',
+    left: 'center'
+},
+tooltip: {
+    trigger: 'item'
+},
 
-                        series: [
-                            {
-                            name: 'Access From',
-                            type: 'pie',
-                            radius: '50%',
-                            data: [
-                                { 
-                                    value: 60,
-                                    name: 'Правильно',
-                                    itemStyle: {
-                                        color: '#00E676'
-                                    }
-                                },
-                                { 
-                                    value: 40, 
-                                    name: 'Неправильно',
-                                    itemStyle: {
-                                        color: '#FB1843'
-                                    }
-                                }
-                            ],
-                            emphasis: {
-                                itemStyle: {
-                                shadowBlur: 10,
-                                shadowOffsetX: 0,
-                                shadowColor: 'rgba(0, 0, 0, 0.5)',
+series: [
+    {
+    name: 'Access From',
+    type: 'pie',
+    radius: '50%',
+    data: [
+        { 
+            value: answers.correct,
+            name: 'Правильно',
+            itemStyle: {
+                color: '#00E676'
+            }
+        },
+        { 
+            value: answers.uncorrect, 
+            name: 'Неправильно',
+            itemStyle: {
+                color: '#FB1843'
+            }
+        }
+    ],
+    emphasis: {
+        itemStyle: {
+        shadowBlur: 10,
+        shadowOffsetX: 0,
+        shadowColor: 'rgba(0, 0, 0, 0.5)',
 
-                                }
-                            }
-                            }
-                        ]
-                        };
+        }
+    }
+    }
+]
+};
 
-                        option && myChart.setOption(option);
+option && myChart.setOption(option);
