5d974ddab40ac98b6c76057e59f72235601a34fb
unknown <av3411261@gmail.com>
2025-09-28 20:43:42 +0300

    Concluded the participants on the teacher_room page

--- quiz_app/static/js/teacher_room.js
+++ quiz_app/static/js/teacher_room.js
@@ -15,7 +15,30 @@
       $("question").innerHTML = `<div><strong>${q.text}</strong></div>`;
     }
 
-        function attachEvents() {
+    function renderParticipants(participants) {
+      const listEl = $("participants-list");
+      const countEl = $("participants-count");
+      listEl.innerHTML = "";
+      countEl.textContent = `Учасники (${participants.length})`;
+      participants.forEach(p => {
+        const name = (p && typeof p === "object")
+          ? (p.nickname || p.name || p.username || p.displayName || "Без имени")
+          : (p || "Без имени");
+        const div = document.createElement("div");
+        div.className = "participant-item-remove-btn";
+        div.innerHTML = `
+          <div class="participant-item">
+            <img src="/quiz/quiz_static/images/profile-avatar.svg" alt="User">
+            <h3>${name}</h3>
+          </div>
+          <img src="/quiz/quiz_static/images/remove-btn.svg" alt="Remove-btn" data-id="${p.id}">
+        `;
+        div.querySelector("img[data-id]").onclick = () => socket.emit("teacher:remove_participant", { code, id: p.id });
+        listEl.appendChild(div);
+      });
+    }
+
+    function attachEvents() {
       $("start").onclick = () => socket.emit("teacher:start", { code });
       $("next").onclick = () => socket.emit("teacher:next", { code });
       $("finish").onclick = () => socket.emit("teacher:finish", { code });
@@ -24,6 +47,7 @@
         $("state").innerText = JSON.stringify(s, null, 2);
         console.log(JSON.stringify(s))
         if (s.question) renderQuestion(s.question);
+        if (s.participants) renderParticipants(s.participants);
       });
 
       socket.on("room:question", (q) => {
@@ -35,7 +59,6 @@
       });
 
       socket.on("room:question_closed", (d) => {
-
         const ans = Array.isArray(d.correct_answer)
           ? d.correct_answer.join(" | ")
           : (d.correct_answer || "не задан");
@@ -47,6 +70,10 @@
         clearUI();
         $("question").innerHTML = `<h3>Results</h3>`;
         $("stats").innerHTML = `<pre>${JSON.stringify(res, null, 2)}</pre>`
+      });
+
+      socket.on("room:participants_updated", (participants) => {
+        renderParticipants(participants);
       });
     }
 
