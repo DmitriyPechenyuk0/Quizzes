19702134e4ba2f4e42fb079a7cd07262e1ee209d
unknown <av3411261@gmail.com>
2025-09-30 18:53:54 +0300

    fixed styles history_sr page

--- history/views.py
+++ history/views.py
@@ -1,12 +1,17 @@
 import flask
+from flask_login import current_user
 
 def show_history_page():
     
     return flask.render_template(template_name_or_list="history.html" )
 
 def show_qsr_page():
-    
-    return flask.render_template(template_name_or_list="quiz_student_result.html" )
+    return flask.render_template(
+        "quiz_student_result.html",
+        page="history",
+        is_auth=current_user.is_authenticated,
+        name=current_user.name if current_user.is_authenticated else "Anonymous"
+    )
 
 def show_qtr_page():
     
