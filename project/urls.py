f5cca55c8db3214aa9f80400b563846f8c92bbef
unknown <av3411261@gmail.com>
2025-10-01 21:53:08 +0300

    fixed icons

--- project/urls.py
+++ project/urls.py
@@ -24,6 +24,10 @@ history.history.add_url_rule(rule='/history', view_func=history.show_history_pag
 history.history.add_url_rule(rule='/history_tr', view_func=history.show_qtr_page, methods= ['POST', 'GET'])
 history.history.add_url_rule(rule='/history_sr', view_func=history.show_qsr_page, methods= ['POST', 'GET'])
 
+# history.history.add_url_rule(rule='history/<int:id>/<int:user_id>', view_func=history.show_qsr_page, methods= ['POST', 'GET'])
+# history.history.add_url_rule(rule='history/<int:id>', view_func=history.show_qsr_page, methods= ['POST', 'GET'])
+
+
 New_Quiz_App.New_Quiz.add_url_rule('/joinnext', view_func=New_Quiz_App.join_next_page, methods = ['POST', 'GET'])
 New_Quiz_App.New_Quiz.add_url_rule(rule= '/new-quiz/<int:id>', view_func=New_Quiz_App.render_new_quiz, methods = ['POST', 'GET'])
 New_Quiz_App.New_Quiz.add_url_rule(rule= '/new-quiz-settings', view_func=New_Quiz_App.render_new_quiz_settigs, methods = ['POST', 'GET'])
